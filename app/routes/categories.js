3f7533d57a65bc26251c1b67c471dafdc564a9bc
Ed Tewiah <etewiah@hotmail.cim>
2014-11-16 14:03:07 +0000

    List categories from discourse

--- app/routes/categories.js
+++ app/routes/categories.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model: function(params) {
+   var url = "/categories.json";
+    // "http://meta.discourse.org/categories.json";
+   // "http://klavado.com/categories.json"
+   var result = $.getJSON(url).then(
+      function(response) {
+      	// debugger;
+      	return response;
+        // return response.data.children.map(function (child) {
+        //   return App.RedditLink.create(child.data);
+        // });
+      }
+    );
+   return result;
+  },
+
+  setupController: function(controller, model) {
+    controller.set('model', model.category_list.categories);
+  }
 });
+
--- config/environment.js
+++ config/environment.js
@@ -43,5 +43,15 @@ module.exports = function(environment) {
 
   }
 
+  // ENV.contentSecurityPolicy = {
+  //   'default-src': "'none'",
+  //   'script-src': "'self' http://meta.discourse.org http://klavado.com", // Allow scripts from https://cdn.mxpnl.com
+  //   'font-src': "'self' http://fonts.gstatic.com", // Allow fonts to be loaded from http://fonts.gstatic.com
+  //   'connect-src': "'self' http://meta.discourse.org http://klavado.com http://custom-api.local", // Allow data (ajax/websocket) from api.mixpanel.com and custom-api.local
+  //   'img-src': "'self'",
+  //   'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com", // Allow inline styles and loaded CSS from http://fonts.googleapis.com 
+  //   'media-src': "'self'"
+  // }
+
   return ENV;
 };
