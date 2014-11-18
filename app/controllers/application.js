b8588dad1f0ec8fb69ea5e1791726b800c061d25
Ed Tewiah <etewiah@hotmail.cim>
2014-11-18 15:00:19 +0000

    Display online status

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,26 +3,34 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 	// http://stackoverflow.com/questions/22122570/ember-js-network-connectivity-check-at-startup-and-listener
 
-
-  isOnline: true,  // assume we're online until proven wrong
+  networkIsOnline: true,  // assume we're online until proven wrong
   init: function () {
     // this.updateNetworkStatus();
   },
 
   updateNetworkStatus: function () {
     var appController = this;
-    if (!navigator.onLine) {
-      this.set('isOnline', false);
-      return; // return early, no point in pinging the server if we have no LAN
-    }
+    // if (!navigator.onLine) {
+    //   this.set('isOnline', false);
+    //   return; // return early, no point in pinging the server if we have no LAN
+    // }
     Ember.$.get('http://happensesame.com').done(function () {
       // todo: consider checking the result
-      appController.set('isOnline', true);
+      appController.set('networkIsOnline', true);
     }).fail(function () {
-      appController.set('isOnline', false);    
+      appController.set('networkIsOnline', false);    
     }).always(function () {
       Ember.run.later(appController, 'updateNetworkStatus', 60000);
     });
-  } 
+  },
+
+  navigatorIsOnline: function(){
+    if (navigator.onLine) {
+      return true;
+    }
+    else{
+      return false;
+    }
+  }.property()
 
 });
