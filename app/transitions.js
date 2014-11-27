29b9cdbfa99e054bc38342c4733c08a5afb45460
Ed Tewiah <etewiah@hotmail.cim>
2014-11-27 22:43:39 +0000

    Improve sites route style

--- app/transitions.js
+++ app/transitions.js
@@ -1,14 +1,27 @@
 import Category from './models/category';
 export default function() {
   this.transition(
-	  // this.betweenModels({instanceOf: Category}),
-   this.hasClass('liquid-category'),
-    this.use('fade',  { delay: 0, duration: 400 })
+    this.hasClass('liquid-directory'),
+    this.use('toRight', {
+      delay: 0,
+      duration: 400
+    })
+  );
+  this.transition(
+    // this.betweenModels({instanceOf: Category}),
+    this.hasClass('liquid-category'),
+    this.use('fade', {
+      delay: 0,
+      duration: 400
+    })
     // this.withinRoute('retriever.site'),
     // this.use('toRight',  { delay: 80, duration: 800 })
   );
   this.transition(
     this.withinRoute('topics.topic.default'),
-    this.use('toRight',  { delay: 80, duration: 800 })
+    this.use('toRight', {
+      delay: 80,
+      duration: 800
+    })
   );
 }
